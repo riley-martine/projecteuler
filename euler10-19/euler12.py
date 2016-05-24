54d17c0a9cc257d32031d07fe2bb0e1a7af34efd
Riley Martine <riley.martine@protonmail.com>
2016-05-24 11:16:22 -0400

    solved 12

--- euler10-19/euler12.py
+++ euler10-19/euler12.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python
 """finds first triangle number to have over five hundred divisors."""
 
+import math
+
 
 def triangle(number):
     """get triangle # number."""
-    sum = 0
-    for i in range(0, number + 1):
-        sum += i
-    return sum
+    return (number * (number + 1)) / 2
 
 
 def numberofdivisors(num):
     """find number of divisors."""
     divs = 0
-    for i in range(1, num + 1):
+    for i in xrange(1, int(math.ceil(math.sqrt(num)))):
         if num % i == 0:
-            divs += 1
+            divs += 2
+        if i*i == num:
+            divs -= 1
     return divs
 
-for COUNT in range(11768, 100000):
+for COUNT in range(2, 100000):
     divvs = numberofdivisors(triangle(COUNT))
     if divvs > 500:
         print triangle(COUNT)
